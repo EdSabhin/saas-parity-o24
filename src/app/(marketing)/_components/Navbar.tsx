89b6cf6df0f7babb03635c7165306622904e031d
EdSabhin <edsabhin@yahoo.com>
2024-10-23 08:48:03 -0500

    Added classes to Navbar header.

--- src/app/(marketing)/_components/Navbar.tsx
+++ src/app/(marketing)/_components/Navbar.tsx
@@ -2,7 +2,7 @@ import { BrandLogo } from "@/components/BrandLogo";
 import Link from "next/link";
 
 export function Navbar() {
-  return <header>
+  return <header className="w-full flex fixed top-0 z-10 bg-background/95 shadow-xl">
     <nav className="container flex items-center gap-10 font-semibold">
       <Link href="/" className="mr-auto">
         <BrandLogo />
@@ -11,7 +11,7 @@ export function Navbar() {
       <Link href="/#pricing" className="text-lg">Pricing</Link>
       <Link href="#" className="text-lg">About</Link>
       <span className="text-lg">
-        
+
       </span>
     </nav>
   </header>
