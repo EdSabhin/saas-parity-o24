eacac756a09ed0127e9feb40a10a690c2535d362
EdSabhin <edsabhin@yahoo.com>
2024-10-23 12:44:26 -0500

    Landing Page first layout done.

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -9,6 +9,7 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { formatCompactNumber } from "@/lib/formatters";
 import { ReactNode } from "react";
 import { cn } from "@/lib/utils";
+import { BrandLogo } from "@/components/BrandLogo";
 
 export default function HomePage() {
   return <>
@@ -66,6 +67,75 @@ export default function HomePage() {
         ))}
       </div>
     </section>
+    <footer className="container flex flex-col sm:flex-row justify-between items-start gap-8 sm:gap-4 pt-16 pb-8">
+      <Link href="/">
+        <BrandLogo />
+      </Link>
+      <div className="flex flex-col sm:flex-row gap-8">
+        <div className="flex flex-col gap-8">
+          <FooterLinkGroup
+            title="Help"
+            links={[
+               { label: "PPP Discounts", href: "#" },
+               { label: "Discount API", href: "#" },
+             ]}
+          />
+          <FooterLinkGroup
+            title="Solutions"
+            links={[
+              { label: "Newsletter", href: "#" },
+              { label: "SaaS Business", href: "#" },
+              { label: "Online Courses", href: "#" },
+              ]}
+            />
+          </div>
+        <div className="flex flex-col gap-8">
+          <FooterLinkGroup
+            title="Features"
+            links={[{ label: "PPP Discounts", href: "#" }]}
+            />
+         <FooterLinkGroup
+            title="Tools"
+            links={[
+                { label: "Salary Converter", href: "#" },
+                { label: "Coupon Generator", href: "#" },
+                { label: "Stripe App", href: "#" },
+              ]}
+            />
+          <FooterLinkGroup
+            title="Company"
+            links={[
+                { label: "Affiliate", href: "#" },
+                { label: "Twitter", href: "#" },
+                { label: "Terms of Service", href: "#" },
+              ]}
+            />
+          </div>
+        <div className="flex flex-col gap-8">
+          <FooterLinkGroup
+            title="Integrations"
+            links={[
+                { label: "Lemon Squeezy", href: "#" },
+                { label: "Gumroad", href: "#" },
+                { label: "Stripe", href: "#" },
+                { label: "Chargebee", href: "#" },
+                { label: "Paddle", href: "#" },
+              ]}
+            />
+            <FooterLinkGroup
+              title="Tutorials"
+              links={[
+                { label: "Any Website", href: "#" },
+                { label: "Lemon Squeezy", href: "#" },
+                { label: "Gumroad", href: "#" },
+                { label: "Stripe", href: "#" },
+                { label: "Chargebee", href: "#" },
+                { label: "Paddle", href: "#" },
+             ]}
+            />
+        </div>
+      </div>
+    </footer>
   </>;
 }
 
@@ -88,11 +158,9 @@ function PricingCard({ name, priceInCents, maxNumberOfVisits, maxNumberOfProduct
       <CardFooter className="flex flex-col items-start gap-4 ">
         <Feature className="font-bold">{maxNumberOfProducts} {maxNumberOfProducts === 1 ? "Product" : "Products"}</Feature>
         <Feature>Cool Feat!</Feature>
-           {canAccessAnalytics && <Feature>Advanced Analytics</Feature>}
+        {canAccessAnalytics && <Feature>Advanced Analytics</Feature>}
         {canCustomizeBanner && <Feature>Banner Customization</Feature>}
         {canRemoveBranding && <Feature>Easy Branding Removal</Feature>}
-     
-        
       </CardFooter>
   </Card>
   )
@@ -105,4 +173,20 @@ function Feature({ children, className}: {children: ReactNode, className?:string
     <span>{children}</span>
   </div>)
   
+}
+
+function FooterLinkGroup({ title, links }: { title: string, links:{label:string, href:string}[]
+}) {
+  return (
+    <div className="flex flex-col gap-4">
+    <h3 className="font-semibold">{title}</h3>
+    <ul className="flex flex-col gap-2 text-sm">
+      {links.map(link => (
+        <li key={link.href}>
+          <Link href={link.href}>{link.label}</Link>
+        </li>
+      ))}
+    </ul>
+  </div>
+  ) 
 }
